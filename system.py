de1d2bed101ca082feb13e03849a53680058ca10
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-01-12 20:59:17 -0300

    Project cration and main system file

---
+++ system.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+while True:
+	answer = int(input('Enter the desired option: '))
+	if answer == 1:
+		# Option to list file contents
+		print(answer)
+	elif answer == 2:
+		# Option to register a new person
+		print(answer)
+	elif answer == 3:
+		print(answer)
+	elif answer == 4:
+		print(answer)
+		break
+	else:
+		print(answer)
+	sleep(1)
