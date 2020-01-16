e558251197c57f2a15512d6c93067032ef78f700
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-01-16 18:35:46 -0300

    Some code structure adjustments

---
+++ file/__init__.py
@@ -0,0 +1,8 @@
+def line(size=42):
+	return '-' * size
+
+
+def header(txt):
+	print(line())
+	print(txt.center(42))
+	print(line())
--- system.py
+++ system.py
@@ -1,3 +1,4 @@
+from interface import *
 from time import sleep
 
 while True:
@@ -7,12 +8,16 @@ while True:
 		print(answer)
 	elif answer == 2:
 		# Option to register a new person
+		header('NEW REGISTER')
 		print(answer)
 	elif answer == 3:
+		# Option to delete a person
 		print(answer)
 	elif answer == 4:
+		# System Exit Option
 		print(answer)
 		break
 	else:
+		# Message if you type any wrong options
 		print(answer)
 	sleep(1)
